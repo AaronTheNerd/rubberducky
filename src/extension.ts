fa44b1ddb9a2a929f76435eee9c833b17b8b13db
AaronTheNerd <bargeaaron@gmail.com>
2025-08-22 09:54:53 -0400

    Feature: Implemented simple chat bot handler

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { BASE_PROMPT } from './prompt';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rubberducky" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('rubberducky.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RubberDucky!');
-	});
-
-	context.subscriptions.push(disposable);
+  const ducky = vscode.chat.createChatParticipant('rubber-ducky', handler);
+  ducky.iconPath = vscode.Uri.joinPath(context.extensionUri, 'assets', 'duck-icon.svg');
 }
 
-// This method is called when your extension is deactivated
+async function handler(
+  request: vscode.ChatRequest,
+  context: vscode.ChatContext,
+  stream: vscode.ChatResponseStream,
+  token: vscode.CancellationToken
+): Promise<void> {
+  let prompt = BASE_PROMPT;
+  const messages = [vscode.LanguageModelChatMessage.User(prompt)];
+  messages.push(vscode.LanguageModelChatMessage.User(request.prompt));
+  const chatResponse = await request.model.sendRequest(messages, {}, token);
+  for await (const fragment of chatResponse.text) {
+    stream.markdown(fragment);
+  }
+  return;
+}
+
 export function deactivate() {}
